530d05e001815425e04b2f76fccfcfa0a3dd346c
Ihle9 <87511620+Ihle9@users.noreply.github.com>
2022-01-24 09:34:30 -0500

    Add files via upload

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)  
+
+    t.window.resizable = false
+    t.window.width = 1920
+    t.window.height = 1080
+    t.console = false
+    t.window.vsync = true
+    t.window.title = "FilianBounce"
+end
